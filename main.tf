c6b96eb0d9056f046d81e55dcf43b622ae57b5fb
vahini <vahini.sriramula@gmail.com>
2026-06-20 12:13:05 +0100

    added my main.tf

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "tf_test" {
+  location = "UKsouth"
+  name = "tf_rg"
+}
+resource "azurerm_container_group" "tfcg_test" {
+  name                = "weatherapi"
+  location            = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
+
+  ip_address_type = "Public"
+  dns_name_label  = "binarythistlewa"
+  os_type         = "Linux"
+
+  container {
+    name   = "weatherapi"
+    image  = "nginx:latest"
+
+    cpu    = 1
+    memory = 1.5
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
